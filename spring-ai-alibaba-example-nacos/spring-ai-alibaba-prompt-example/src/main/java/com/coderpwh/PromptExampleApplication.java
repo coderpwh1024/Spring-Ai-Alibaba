4d442a24119b24b05862c138a92cb70c9512e046
pengwenhao <pengwenhao@aliyun.com>
2025-01-15 14:51:05 +0800

    fix: add  nacos

--- spring-ai-alibaba-example-nacos/spring-ai-alibaba-prompt-example/src/main/java/com/coderpwh/PromptExampleApplication.java
+++ spring-ai-alibaba-example-nacos/spring-ai-alibaba-prompt-example/src/main/java/com/coderpwh/PromptExampleApplication.java
@@ -2,9 +2,12 @@ package com.coderpwh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @author coderpwh
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PromptExampleApplication {
 
