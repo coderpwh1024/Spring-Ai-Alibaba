52810e2b7c5a4c8da4ab05df9ce6a99a69b776ac
pengwenhao <pengwenhao@aliyun.com>
2025-01-15 14:35:26 +0800

    feat: add data and resource

---
+++ spring-ai-alibaba-example-nacos/spring-ai-alibaba-prompt-example/src/main/java/com/coderpwh/controller/RoleController.java
@@ -0,0 +1,60 @@
+package com.coderpwh.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author coderpwh
+ */
+@RestController
+@RequestMapping("/example/ai")
+public class RoleController {
+
+    @Value("classpath:/prompts/system-message.st")
+    private Resource systemResource;
+
+    private final ChatClient chatClient;
+
+    @Autowired
+    public RoleController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+
+    @GetMapping("/roles")
+    public AssistantMessage generate(
+            @RequestParam(value = "message",
+                    defaultValue = "Tell me about three famous pirates from the Golden Age of Piracy and why they did.  Write at least a sentence for each pirate.") String message,
+            @RequestParam(value = "name", defaultValue = "Bob") String name,
+            @RequestParam(value = "voice", defaultValue = "pirate") String voice
+    ) {
+
+        UserMessage userMessage = new UserMessage(message);
+
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemResource);
+        Message systemMessage = systemPromptTemplate.createMessage(Map.of("name", name, "voice", voice));
+
+        return chatClient.prompt(new Prompt(List.of(userMessage, systemMessage)))
+                .call()
+                .chatResponse()
+                .getResult()
+                .getOutput();
+    }
+
+
+
+}
---
+++ spring-ai-alibaba-example-nacos/spring-ai-alibaba-prompt-example/src/main/java/com/coderpwh/controller/StuffController.java
@@ -0,0 +1,64 @@
+package com.coderpwh.controller;
+
+
+import com.coderpwh.entity.Completion;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author coderpwh
+ */
+@RestController
+@RequestMapping("/example/ai")
+public class StuffController {
+
+
+    @Value("classpath:/docs/wikipedia-curling.md")
+    private Resource docsToStuffResource;
+
+    @Value("classpath:/prompts/qa-prompt.st")
+    private Resource qaPromptResource;
+
+    private final ChatClient chatClient;
+
+    @Autowired
+    public StuffController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping(value = "/stuff")
+    public Completion completion(
+            @RequestParam(value = "message",
+                    defaultValue = "Which athletes won the mixed doubles gold medal in curling at the 2022 Winter Olympics?'") String message,
+            @RequestParam(value = "stuffit", defaultValue = "false") boolean stuffit
+    ) {
+
+        PromptTemplate promptTemplate = new PromptTemplate(qaPromptResource);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("question", message);
+        if (stuffit) {
+            map.put("context", docsToStuffResource);
+        } else {
+            map.put("context", "");
+        }
+
+        return new Completion(
+                chatClient.prompt(promptTemplate.create(map))
+                        .call()
+                        .content()
+        );
+    }
+
+
+}
