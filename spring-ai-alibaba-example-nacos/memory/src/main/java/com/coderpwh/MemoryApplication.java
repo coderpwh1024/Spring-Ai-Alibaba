09a377ba52bfc7f8bf8500bb47f06e0149c844f7
pengwenhao <pengwenhao@aliyun.com>
2025-03-17 17:58:24 +0800

    feat:init memory

---
+++ spring-ai-alibaba-example-nacos/memory/src/main/java/com/coderpwh/MemoryApplication.java
@@ -0,0 +1,12 @@
+package com.coderpwh;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author coderpwh
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MemoryApplication {
+}
