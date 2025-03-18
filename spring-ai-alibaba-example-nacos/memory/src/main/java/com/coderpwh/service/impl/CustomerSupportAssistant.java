be3b0081086b1ec34ebbfd095cd0addfeed9ed7a
pengwenhao <pengwenhao@aliyun.com>
2025-03-18 11:11:12 +0800

    feat:add Azure

--- spring-ai-alibaba-example-nacos/memory/src/main/java/com/coderpwh/service/impl/CustomerSupportAssistant.java
+++ spring-ai-alibaba-example-nacos/memory/src/main/java/com/coderpwh/service/impl/CustomerSupportAssistant.java
@@ -1,2 +1,52 @@
-package com.coderpwh.service.impl;public class CustomerSupportAssistant {
+package com.coderpwh.service.impl;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+/**
+ * @author coderpwh
+ */
+@Service
+public class CustomerSupportAssistant {
+
+    private final ChatClient chatClient;
+
+    public CustomerSupportAssistant(ChatClient.Builder builder, VectorStore vectorStore, ChatMemory chatMemory) {
+
+        this.chatClient = builder
+                .defaultSystem("""
+                        You are a customer chat support agent of an airline named "Funnair". Respond in a friendly,
+                        helpful, and joyful manner.
+
+                        Before providing information about a booking or cancelling a booking, you MUST always
+                        get the following information from the user: booking number, customer first name and last name.
+
+                        Before changing a booking you MUST ensure it is permitted by the terms.
+
+                        If there is a charge for the change, you MUST ask the user to consent before proceeding.
+                        """)
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        new QuestionAnswerAdvisor(vectorStore),
+                        new SimpleLoggerAdvisor())
+                .defaultFunctions("getBookingDetails", "changeBooking", "cancelBooking")
+                .build();
+    }
+
+    public Flux<String> chat(String chatId, String userMessageContent) {
+
+        return this.chatClient.prompt()
+                .user(userMessageContent)
+                .advisors(a -> a
+                        .param("", chatId)
+                        .param("", 100))
+                .stream().content();
+    }
+
 }
