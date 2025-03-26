eb4270fcd032daf28acf35115669e031500f6d43
pengwenhao <pengwenhao@aliyun.com>
2025-03-26 10:28:00 +0800

    feat:add openmanus

--- spring-ai-alibaba-example-nacos/openmanus/src/main/java/com/coderpwh/llm/ToolBuilder.java
+++ spring-ai-alibaba-example-nacos/openmanus/src/main/java/com/coderpwh/llm/ToolBuilder.java
@@ -6,6 +6,12 @@ import java.util.List;
 import com.coderpwh.agent.BaseAgent;
 
 
+import com.coderpwh.tool.BrowserUseTool;
+import com.coderpwh.tool.FileSaver;
+import com.coderpwh.tool.GoogleSearch;
+import com.coderpwh.tool.PlanningTool;
+import com.coderpwh.tool.PythonExecute;
+import com.coderpwh.tool.Summary;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.model.function.FunctionCallback;
 import org.springframework.ai.tool.ToolCallback;
