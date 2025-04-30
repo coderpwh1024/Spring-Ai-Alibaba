d4f76391c010c6ac4b1183e28e2d9dab15387682
pengwenhao <pengwenhao@aliyun.com>
2025-04-30 10:23:44 +0800

    feat:init McpServer

---
+++ spring-ai-alibaba-example-nacos/mcp/start/server/starter-stdio-server/src/main/java/com/coderpwh/McpServerApplication.java
@@ -0,0 +1,17 @@
+package com.coderpwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author coderpwh
+ */
+
+@SpringBootApplication
+public class McpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpServerApplication.class, args);
+    }
+
+}
