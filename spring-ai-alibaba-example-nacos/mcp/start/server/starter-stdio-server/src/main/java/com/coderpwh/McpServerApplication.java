2ffb283c133171990f269085cf8002c72af6bdf6
pengwenhao <pengwenhao@aliyun.com>
2025-05-06 15:21:25 +0800

    fix:update mcp

--- spring-ai-alibaba-example-nacos/mcp/start/server/starter-stdio-server/src/main/java/com/coderpwh/McpServerApplication.java
+++ spring-ai-alibaba-example-nacos/mcp/start/server/starter-stdio-server/src/main/java/com/coderpwh/McpServerApplication.java
@@ -1,7 +1,11 @@
 package com.coderpwh;
 
+import com.coderpwh.service.OpenMeteoService;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author coderpwh
@@ -13,5 +17,12 @@ public class McpServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(McpServerApplication.class, args);
     }
+
+
+    @Bean
+    public ToolCallbackProvider weatherTools(OpenMeteoService openMeteoService) {
+        return MethodToolCallbackProvider.builder().toolObjects(openMeteoService).build();
+    }
+
 
 }
