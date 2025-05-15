241e0a841e2028ba6f45cc014b9c1b14e5d45b97
pengwenhao <pengwenhao@aliyun.com>
2025-05-15 18:26:23 +0800

    feat:add RewordingNode

---
+++ spring-ai-alibaba-example-nacos/graph/src/main/java/com/coderpwh/graph/node/RewordingNode.java
@@ -0,0 +1,37 @@
+package com.coderpwh.graph.node;
+
+import com.alibaba.cloud.ai.graph.OverAllState;
+import com.alibaba.cloud.ai.graph.action.NodeAction;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author coderpwh
+ */
+public class RewordingNode implements NodeAction {
+
+
+    private final ChatClient chatClient;
+
+
+    public RewordingNode(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @Override
+    public Map<String, Object> apply(OverAllState state) throws Exception {
+        String summary = (String) state.value("summary").orElse("");
+        String prompt = "请将以下摘要用更优美、生动的语言改写，同时保持信息不变：\n\n" + summary;
+
+        ChatResponse response = chatClient.prompt(prompt).call().chatResponse();
+        String reworded = response.getResult().getOutput().getText();
+
+        Map<String,Object> result  = new HashMap<>();
+        result.put("reworded",reworded);
+        return result;
+    }
+
+}
