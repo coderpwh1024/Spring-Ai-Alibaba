c339c2e1fcc27d9948dc8ccf647c18d937f0b0b9
pengwenhao <pengwenhao@aliyun.com>
2025-05-23 11:12:38 +0800

    fix:update application

--- spring-ai-alibaba-example-nacos/mcp/start/server/starter-webflux-server/src/main/java/com/coderpwh/controller/OpenMeteoController.java
+++ spring-ai-alibaba-example-nacos/mcp/start/server/starter-webflux-server/src/main/java/com/coderpwh/controller/OpenMeteoController.java
@@ -2,9 +2,12 @@ package com.coderpwh.controller;
 
 import com.coderpwh.service.OpenMeteoService;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @RestController
 public class OpenMeteoController {
@@ -12,11 +15,10 @@ public class OpenMeteoController {
     @Resource
     private OpenMeteoService openMeteoService;
 
-    @RequestMapping(value = "/see", method = RequestMethod.GET)
-    public Object getWeatherInfo() {
-        String str = "这是天气";
-
-        return str;
+    @GetMapping(value = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> sse() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> "event: message\ndata: {\"content\": \"Event " + sequence + "\"}\n");
     }
 
 }
