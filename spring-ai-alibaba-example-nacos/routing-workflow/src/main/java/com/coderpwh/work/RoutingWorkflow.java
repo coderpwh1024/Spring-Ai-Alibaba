8fa7cd4a69e68f61909fda129ba78960622021cf
pengwenhao <pengwenhao@aliyun.com>
2025-06-05 14:51:12 +0800

    feat:add RoutingWorkflow

--- spring-ai-alibaba-example-nacos/routing-workflow/src/main/java/com/coderpwh/work/RoutingWorkflow.java
+++ spring-ai-alibaba-example-nacos/routing-workflow/src/main/java/com/coderpwh/work/RoutingWorkflow.java
@@ -1,4 +1,57 @@
 package com.coderpwh.work;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.util.Assert;
+
+import java.util.Map;
+
 public class RoutingWorkflow {
+
+    private final ChatClient chatClient;
+
+    public RoutingWorkflow(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    public String route(String input, Map<String, String> routes) {
+        Assert.notNull(input, "Input text cannot be null");
+        Assert.notEmpty(routes, "Routes map cannot be null or empty");
+
+
+        String routeKey = determineRoute(input, routes.keySet());
+        String selectedPrompt = routes.get(routeKey);
+
+        if (selectedPrompt == null) {
+            throw new IllegalArgumentException("Selected route '" + routeKey + "' not found in routes map");
+        }
+
+        return chatClient.prompt(selectedPrompt + "\nInput: " + input).call().content();
+    }
+
+
+    @SuppressWarnings("null")
+    private String determineRoute(String input, Iterable<String> availableRoutes) {
+        System.out.println("\nAvailable routes: " + availableRoutes);
+
+        String selectorPrompt = String.format("""
+                Analyze the input and select the most appropriate support team from these options: %s
+                First explain your reasoning, then provide your selection in this JSON format:
+
+                \\{
+                    "reasoning": "Brief explanation of why this ticket should be routed to a specific team.
+                                Consider key terms, user intent, and urgency level.",
+                    "selection": "The chosen team name"
+                \\}
+
+                Input: %s""", availableRoutes, input);
+
+        RoutingResponse routingResponse = chatClient.prompt(selectorPrompt).call().entity(RoutingResponse.class);
+
+        System.out.println(String.format("Routing Analysis:%s\nSelected route: %s",
+                routingResponse.reasoning(), routingResponse.selection()));
+
+        return routingResponse.selection();
+    }
+
+
 }
