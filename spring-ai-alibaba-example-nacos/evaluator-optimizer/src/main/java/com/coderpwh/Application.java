2f4193f23aca0c851057f725cb04dde13c0f40ec
pengwenhao <pengwenhao@aliyun.com>
2025-06-06 10:22:49 +0800

    fix:update Application

--- spring-ai-alibaba-example-nacos/evaluator-optimizer/src/main/java/com/coderpwh/Application.java
+++ spring-ai-alibaba-example-nacos/evaluator-optimizer/src/main/java/com/coderpwh/Application.java
@@ -1,7 +1,11 @@
 package com.coderpwh;
 
+import com.coderpwh.work.EvaluatorOptimizer;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author coderpwh
@@ -9,11 +13,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ChatClient.Builder chatClientBuilder) {
+        var chatClient = chatClientBuilder.build();
+        return args -> {
+
+            EvaluatorOptimizer.RefinedResponse refinedResponse = new EvaluatorOptimizer(chatClient)
+                    .loop("""
+                            <user input>
+                            Implement a Stack in Java with:
+                            1. push(x)
+                            2. pop()
+                            3. getMin()
+                            All operations should be O(1).
+                            All inner fields should be private and when used should be prefixed with 'this.'.
+                            </user input>
+                            """);
+
+            System.out.println("FINAL OUTPUT:\n:" + refinedResponse);
+
+        };
+
+    }
 
 
 }
