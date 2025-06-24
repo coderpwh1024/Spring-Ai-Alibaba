eb34c5326c9d937eea56b0b8ed1e317dfbad854d
pengwenhao <pengwenhao@aliyun.com>
2025-06-24 15:39:38 +0800

    fix:update RagApplication

--- spring-ai-alibaba-example-nacos/milvus-rag/src/main/java/com/coderpwh/RagApplication.java
+++ spring-ai-alibaba-example-nacos/milvus-rag/src/main/java/com/coderpwh/RagApplication.java
@@ -1,7 +1,16 @@
 package com.coderpwh;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author coderpwh
  */
+@SpringBootApplication
 public class RagApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RagApplication.class, args);
+    }
+
+
 }
