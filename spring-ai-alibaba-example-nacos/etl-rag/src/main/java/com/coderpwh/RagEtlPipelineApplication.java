df83686a72add7b76c4f1ea1ee7ae266d68a85e1
pengwenhao <pengwenhao@aliyun.com>
2025-06-26 13:51:57 +0800

    feat:init Constant

---
+++ spring-ai-alibaba-example-nacos/etl-rag/src/main/java/com/coderpwh/RagEtlPipelineApplication.java
@@ -0,0 +1,19 @@
+package com.coderpwh;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author coderpwh
+ */
+@SpringBootApplication
+public class RagEtlPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagEtlPipelineApplication.class, args);
+
+    }
+
+
+}
