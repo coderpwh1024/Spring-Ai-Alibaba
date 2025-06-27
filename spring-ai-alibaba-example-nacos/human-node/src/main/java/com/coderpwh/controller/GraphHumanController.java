707004f329f157feae489699ab59381e9a5badc9
pengwenhao <pengwenhao@aliyun.com>
2025-06-27 18:12:36 +0800

    feat:init GraphHumanController

--- spring-ai-alibaba-example-nacos/human-node/src/main/java/com/coderpwh/controller/GraphHumanController.java
+++ spring-ai-alibaba-example-nacos/human-node/src/main/java/com/coderpwh/controller/GraphHumanController.java
@@ -1,7 +1,66 @@
 package com.coderpwh.controller;
 
+import com.alibaba.cloud.ai.graph.CompileConfig;
+import com.alibaba.cloud.ai.graph.CompiledGraph;
+import com.alibaba.cloud.ai.graph.NodeOutput;
+import com.alibaba.cloud.ai.graph.RunnableConfig;
+import com.alibaba.cloud.ai.graph.StateGraph;
+import com.alibaba.cloud.ai.graph.async.AsyncGenerator;
+import com.alibaba.cloud.ai.graph.checkpoint.config.SaverConfig;
+import com.alibaba.cloud.ai.graph.checkpoint.constant.SaverConstant;
+import com.alibaba.cloud.ai.graph.checkpoint.savers.MemorySaver;
+import com.alibaba.cloud.ai.graph.exception.GraphRunnerException;
+import com.alibaba.cloud.ai.graph.exception.GraphStateException;
+import com.coderpwh.controller.GraphProcess.GraphProcess;
+import org.apache.commons.math3.analysis.function.Sin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author coderpwh
  */
+@RestController
+@RequestMapping("/graph/human")
 public class GraphHumanController {
+
+    private static final Logger logger = LoggerFactory.getLogger(GraphHumanController.class);
+
+    private final CompiledGraph compiledGraph;
+
+    @Autowired
+    public GraphHumanController(@Qualifier("humanGraph") StateGraph stateGraph) throws GraphStateException {
+        SaverConfig saverConfig = SaverConfig.builder().register(SaverConstant.MEMORY, new MemorySaver()).build();
+        this.compiledGraph = stateGraph.compile(CompileConfig.builder().saverConfig(saverConfig).interruptBefore("human_feedback").build());
+    }
+
+    public Flux<ServerSentEvent<String>> expand(@RequestParam(value = "query", defaultValue = "你好，很高兴认识你，能简单介绍一下自己吗？", required = false) String query,
+                                                @RequestParam(value = "expander_number", defaultValue = "3", required = false) Integer expanderNumber,
+                                                @RequestParam(value = "thread_id", defaultValue = "yingzi", required = false) String threadId) throws GraphRunnerException {
+
+        RunnableConfig runnableConfig = RunnableConfig.builder().threadId(threadId).build();
+        Map<String, Object> objectMap = new HashMap<>();
+        objectMap.put("query", query);
+        objectMap.put("expander_number", expanderNumber);
+
+        GraphProcess graphProcess = new GraphProcess(this.compiledGraph);
+        Sinks.Many<ServerSentEvent<String>> sink = Sinks.many().unicast().onBackpressureBuffer();
+
+        AsyncGenerator<NodeOutput> resultFuture = compiledGraph.stream(objectMap, runnableConfig);
+
+        return sink.asFlux().doOnCancel(() -> logger.info("Client disconnected from stream"))
+                .doOnError(e -> logger.error("Error occurred while processing stream:", e));
+    }
+
+
 }
