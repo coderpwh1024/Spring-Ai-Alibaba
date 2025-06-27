c48d4c35504f7b099d7580dc12d6b3fcf107bf28
pengwenhao <pengwenhao@aliyun.com>
2025-06-27 16:47:35 +0800

    feat:init TranslateNode

---
+++ spring-ai-alibaba-example-nacos/human-node/src/main/java/com/coderpwh/config/GraphHumanConfiguration.java
@@ -0,0 +1,23 @@
+package com.coderpwh.config;
+
+import com.alibaba.cloud.ai.graph.StateGraph;
+import com.alibaba.cloud.ai.graph.exception.GraphStateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author coderpwh
+ */
+@Configuration
+public class GraphHumanConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(GraphHumanConfiguration.class);
+
+
+
+
+
+
+}
