570beedd0ea15df8622f047e0ca6cfb3d0dc2df6
pengwenhao <pengwenhao@aliyun.com>
2025-07-25 11:24:34 +0800

    feat:init McpNode

--- spring-ai-alibaba-example-nacos/graph_demo/mcp-node/src/main/java/com/coderpwh/node/McpNode.java
+++ spring-ai-alibaba-example-nacos/graph_demo/mcp-node/src/main/java/com/coderpwh/node/McpNode.java
@@ -2,11 +2,16 @@ package com.coderpwh.node;
 
 import com.alibaba.cloud.ai.graph.OverAllState;
 import com.alibaba.cloud.ai.graph.action.NodeAction;
+import com.coderpwh.tool.McpClientToolCallbackProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.tool.ToolCallback;
+import reactor.core.publisher.Flux;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author coderpwh
@@ -14,22 +19,34 @@ import java.util.Map;
 
 public class McpNode implements NodeAction {
 
-    private static  final Logger logger = LoggerFactory.getLogger(McpNode.class);
+    private static final Logger logger = LoggerFactory.getLogger(McpNode.class);
 
 
-    private  static  final  String NODE_NAME = "mcp-node";
+    private static final String NODE_NAME = "mcp-node";
 
 
-//    private  final ChatClient chatClient;
+    private final ChatClient chatClient;
 
 
-
-
+    public McpNode(ChatClient.Builder chatClientBuilder, McpClientToolCallbackProvider mcpClientToolCallbackProvider) {
+        Set<ToolCallback> toolCallbacks = mcpClientToolCallbackProvider.findToolCallbacks(NODE_NAME);
+        for (ToolCallback toolCallback : toolCallbacks) {
+            logger.info("mcp-node find toolCallback:{}", toolCallback.getToolDefinition().name());
+        }
+        this.chatClient = chatClientBuilder.defaultToolCallbacks(toolCallbacks.toArray(ToolCallback[]::new)).build();
+    }
 
 
     @Override
     public Map<String, Object> apply(OverAllState state) throws Exception {
-        return null;
+        String query = state.value("query", "");
+        Flux<String> streamResult = chatClient.prompt(query).stream().content();
+        String result = streamResult.reduce("", (acc, item) -> acc + item).block();
+
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap.put("mcp_content", result);
+
+        return resultMap;
     }
 
 
