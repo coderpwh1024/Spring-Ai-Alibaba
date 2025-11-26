fea99bb9849604a11fbde4936f650d9ae16f4ed3
coderpwh1024 <pengwenhao@aliyun.com>
2025-11-26 10:15:02 +0800

    feat:init intercerptor

--- spring-ai-alibaba-example-nacos/agent/react-agent/src/main/java/com/coderpwh/config/AgentConfiguration.java
+++ spring-ai-alibaba-example-nacos/agent/react-agent/src/main/java/com/coderpwh/config/AgentConfiguration.java
@@ -1,5 +1,11 @@
 package com.coderpwh.config;
 
+import com.alibaba.cloud.ai.graph.agent.ReactAgent;
+import com.alibaba.cloud.ai.graph.agent.hook.hip.HumanInTheLoopHook;
+import com.alibaba.cloud.ai.graph.checkpoint.savers.MemorySaver;
+import com.alibaba.cloud.ai.graph.exception.GraphStateException;
+import com.coderpwh.tools.FileReadTool;
+import com.coderpwh.tools.FileWriteTool;
 import org.springframework.ai.chat.model.ChatModel;
 
 /**
@@ -8,16 +14,31 @@ import org.springframework.ai.chat.model.ChatModel;
 public class AgentConfiguration {
 
 
-     private  final ChatModel chatModel;
+    private final ChatModel chatModel;
 
 
-     public AgentConfiguration(ChatModel chatModel) {
-         this.chatModel = chatModel;
-     }
+    public AgentConfiguration(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
 
 
+   /* public ReactAgent reactAgent() throws GraphStateException {
+        return  ReactAgent
+                .builder()
+                .name("agent")
+                .description("This is a react agent")
+                .model(chatModel)
+                .saver(new MemorySaver())
+                .tools(
+                        new FileReadTool().toolCallback(),
+                        new FileWriteTool().toolCallback()
+                ).hooks(HumanInTheLoopHook.builder().approvalOn("file_write", "Write File should be approved")
+                        .build())
+                .interceptors()
 
 
+    }
+*/
 
 
 
